8fa08eba7917737f3fe8e7675dc45b37e2e685c1
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2021-12-30 15:47:35 +0530

    Add. Helloworld app with functional Components

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function Greet() {
+//     return <h1>Hello Sakshi</h1>
+// }
+
+//Using Arrow Functions
+const Greet = () => <h1>Hello, Welcome to the program</h1>
+
+export default Greet
